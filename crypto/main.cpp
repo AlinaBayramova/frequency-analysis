438923586cafe7b867fdfda1433211711755d815
Alina Bairamova <Alina@macbook-air-alina-2.local>
2018-09-10 16:24:18 +0300

    Initial Commit

---
+++ crypto/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  crypto
+//
+//  Created by Alina Bairamova on 10.09.2018.
+//  Copyright © 2018 non. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
